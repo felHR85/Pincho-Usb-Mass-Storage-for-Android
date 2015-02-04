9928c9099d69702336430e5d21c8bea5feff610b
Felipe Herranz <felipe.herranz@gmail.com>
2015-02-04 18:41:13 +0100

    parsePartition completed

--- src/main/java/com/felhr/usbmassstorageforandroid/filesystems/MasterBootRecord.java
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/MasterBootRecord.java
@@ -6,6 +6,7 @@ package com.felhr.usbmassstorageforandroid.filesystems;
 public class MasterBootRecord
 {
     private byte[] codeArea;
+    private int diskSignature;
     private Partition[] partitions;
 
     public MasterBootRecord()
@@ -13,7 +14,7 @@ public class MasterBootRecord
 
     }
 
-    private static MasterBootRecord parseMbr(byte[] data)
+    public static MasterBootRecord parseMbr(byte[] data)
     {
         if(data.length == 512)
         {
@@ -23,6 +24,41 @@ public class MasterBootRecord
             return null; // MBR must be 512 length
         }
        return null;
+    }
+
+    private static Partition[] parsePartitionTable(byte[] partitionTable)
+    {
+        Partition[] partitions = new Partition[getPartitionsCount(partitionTable)];
+        int partitionIndex = 0;
+        if(partitionTable.length == 64)
+        {
+            for(int startIndex = 0;startIndex<= 48;startIndex+=16)
+            {
+                byte[] partitionData = new byte[16];
+                System.arraycopy(partitionTable, startIndex, partitionData, 0, 16);
+                Partition partition = Partition.parsePartition(partitionData);
+                if(partition != null)
+                    partitions[partitionIndex] = partition;
+            }
+            return partitions;
+
+        }else
+        {
+            return null; // Partition table must be 64 byte length
+        }
+    }
+
+    private static int getPartitionsCount(byte[] partitionTable)
+    {
+        int index = 0;
+        int counter = 0;
+        while(index <= 48)
+        {
+            if(partitionTable[index + 4] != 0x00)
+                counter++;
+            index += 16;
+        }
+        return counter;
     }
 
 }
--- src/main/java/com/felhr/usbmassstorageforandroid/filesystems/Partition.java
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/Partition.java
@@ -1,25 +1,79 @@
 package com.felhr.usbmassstorageforandroid.filesystems;
 
+import com.felhr.usbmassstorageforandroid.utilities.EndianessUtil;
+
 /**
  * Created by Felipe Herranz(felhr85@gmail.com) on 4/2/15.
  */
 public class Partition
 {
     private boolean bootable;
-    private int cylinderHeadSectorAddressFirst;
-    private int partitionType;
-    private int cylinderHeadSectorAddressLast;
-    private int lbaStart;
-    private int sectorsNumber;
+    private int chsStart; // 3-bytes
+    private int partitionType; // 1-byte
+    private int chsEnd; // 3-bytes
+    private int lbaStart; // 4-bytes
+    private int sectorsNumber; // 4-bytes
 
-    public Partition()
+    private Partition()
     {
 
     }
 
-    public static Partition parsePartitionTable(byte[] partitionTable)
+    public static Partition parsePartition(byte[] partitionData)
     {
-        // TODO
-        return null;
+        if(partitionData.length == 16 && !isEmptyPartition(partitionData))
+        {
+            Partition partition = new Partition();
+            partition.bootable = partitionData[0] == 0x80;
+            partition.chsStart = partitionData[3] << 16 + partitionData[2] << 8 + partitionData[1];
+            partition.partitionType = partitionData[4];
+            partition.chsEnd = partitionData[7] << 16 + partitionData[6] << 8 + partitionData[5];
+            partition.lbaStart = partitionData[11] << 24 + partitionData[10] << 16 + partitionData[9] << 8 + partitionData[8];
+            partition.sectorsNumber = partitionData[15] << 24 + partitionData[14] << 16 + partitionData[13] << 8 + partitionData[12];
+            return partition;
+        }else
+        {
+            return null;
+        }
+    }
+
+    private static boolean isEmptyPartition(byte[] partition)
+    {
+        return partition[4] == 0x00;
+    }
+
+    public boolean isBootable()
+    {
+        return bootable;
+    }
+
+
+    public int getChsStart()
+    {
+        return chsStart;
+    }
+
+
+    public int getPartitionType()
+    {
+        return partitionType;
+    }
+
+
+    public int getChsEnd()
+    {
+        return chsEnd;
+    }
+
+
+    public int getLbaStart()
+    {
+        return lbaStart;
+    }
+
+
+    public int getSectorsNumber()
+    {
+        return sectorsNumber;
     }
 }
