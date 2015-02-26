09fe940bf1ba54a5fc9d67f3bab9a682d7481d9a
Felipe Herranz <felipe.herranz@gmail.com>
2015-02-26 23:19:29 +0100

    Changed the byte buffer for a dynamic array workaround

--- src/main/java/com/felhr/usbmassstorageforandroid/scsi/SCSIRead10Response.java
+++ src/main/java/com/felhr/usbmassstorageforandroid/scsi/SCSIRead10Response.java
@@ -1,6 +1,7 @@
 package com.felhr.usbmassstorageforandroid.scsi;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.felhr.usbmassstorageforandroid.utilities.HexUtil;
 
@@ -9,17 +10,17 @@ import com.felhr.usbmassstorageforandroid.utilities.HexUtil;
  */
 public class SCSIRead10Response extends SCSIResponse
 {
-    private byte[] readBuffer;
+    private DynamicBuffer readBuffer;
 
     private SCSIRead10Response()
     {
-
+        this.readBuffer = new DynamicBuffer(512, 2);
     }
 
     public static SCSIRead10Response getResponse(byte[] data)
     {
         SCSIRead10Response response = new SCSIRead10Response();
-        response.readBuffer = data;
+        response.addToBuffer(data);
         return response;
     }
 
@@ -27,19 +28,58 @@ public class SCSIRead10Response extends SCSIResponse
     public Bundle getReadableResponse()
     {
         Bundle bundle = new Bundle();
-        bundle.putString("readBuffer", HexUtil.hexToString(readBuffer));
+        byte[] buffer = readBuffer.getBuffer();
+        Log.i("length buffer", String.valueOf(buffer.length));
+        bundle.putString("readBuffer", HexUtil.hexToString(buffer));
         return bundle;
     }
 
-
-
-    public byte[] getReadBuffer()
+    private void addToBuffer(byte[] data)
     {
-        return readBuffer;
+        readBuffer.addElements(data);
     }
 
-    public void setReadBuffer(byte[] readBuffer)
+    private byte[] getBuffer()
     {
-        this.readBuffer = readBuffer;
+        return readBuffer.getBuffer();
+    }
+
+    private class DynamicBuffer
+    {
+        private byte[] buffer;
+        private int pointer;
+        private int resizeFactor;
+
+        public DynamicBuffer(int size, int resizeFactor)
+        {
+            this.buffer = new byte[size];
+            this.pointer = 0;
+            this.resizeFactor = resizeFactor;
+        }
+
+        public void addElements(byte[] newBuffer)
+        {
+            if((pointer + newBuffer.length) <= buffer.length)
+            {
+                System.arraycopy(newBuffer, 0, buffer, pointer, newBuffer.length);
+                pointer = pointer + newBuffer.length;
+            }else // Resize buffer
+            {
+                byte[] resizedBuffer = new byte[resizeFactor * buffer.length];
+                pointer = buffer.length;
+                System.arraycopy(buffer, 0, resizedBuffer, 0, buffer.length);
+                buffer = resizedBuffer;
+            }
+        }
+
+        public byte[] getBuffer()
+        {
+            return buffer;
+        }
+
+        private void resizeBuffer()
+        {
+
+        }
     }
 }
