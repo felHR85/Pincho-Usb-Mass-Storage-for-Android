f6c2f00dc4786516b9a5a925a4c9e17eb9f6d9a1
Felipe Herranz <felhr85@gmail.com>
2015-06-10 23:11:54 +0200

    Added a new layer of abstraction above FatHandler

---
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VFSFile.java
@@ -0,0 +1,8 @@
+package com.felhr.usbmassstorageforandroid.filesystems;
+
+/**
+ * Created by Felipe Herranz(felhr85@gmail.com) on 10/6/15.
+ */
+public class VFSFile
+{
+}
---
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VirtualFileSystem.java
@@ -0,0 +1,69 @@
+package com.felhr.usbmassstorageforandroid.filesystems;
+
+import java.util.List;
+
+/**
+ * Created by Felipe Herranz(felhr85@gmail.com) on 10/6/15.
+ */
+public class VirtualFileSystem
+{
+    public VirtualFileSystem()
+    {
+        //TODO
+    }
+
+    public boolean mount(int index)
+    {
+        //TODO
+        return false;
+    }
+
+    public List<String> list()
+    {
+        //TODO
+        return null;
+    }
+
+    public List<VFSFile> listAll()
+    {
+        //TODO
+        return null;
+    }
+
+    public String getPath()
+    {
+        //TODO
+        return null;
+    }
+
+    public boolean changeDir(String dirName)
+    {
+        //TODO
+        return false;
+    }
+
+    public boolean changeDirBack()
+    {
+        //TODO
+        return false;
+    }
+
+    public boolean writeFile()
+    {
+        //TODO
+        return false;
+    }
+
+    public byte[] readFile(String fileName)
+    {
+        //TODO
+        return null;
+    }
+
+    public boolean unMount()
+    {
+        //TODO
+        return false;
+    }
+
+}
