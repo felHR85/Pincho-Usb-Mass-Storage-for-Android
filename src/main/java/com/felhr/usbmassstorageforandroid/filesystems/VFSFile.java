75a58336f67a1723d86349bfd64195611ce9af6e
Felipe Herranz <felhr85@gmail.com>
2015-06-11 18:51:04 +0200

    First version of the frontend implemented

--- src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VFSFile.java
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VFSFile.java
@@ -1,8 +1,190 @@
 package com.felhr.usbmassstorageforandroid.filesystems;
 
+import com.felhr.usbmassstorageforandroid.filesystems.fat32.FileEntry;
+
+import java.util.Date;
+
 /**
  * Created by Felipe Herranz(felhr85@gmail.com) on 10/6/15.
  */
+
+/*
+  VFSFile implements an abstraction over FAT entries, hiding the cluster pointer and other stuff
+ */
 public class VFSFile
 {
+    private String fileName;
+    private String fileExtension;
+    private boolean isReadOnly;
+    private boolean isHidden;
+    private boolean isSystem;
+    private boolean isVolume;
+    private boolean isDirectory;
+    private boolean isArchive;
+    private Date creationDate;
+    private Date lastAccessedDate;
+    private Date lastModifiedDate;
+    private long size;
+
+    public VFSFile()
+    {
+
+    }
+
+    public VFSFile(String fileName, String fileExtension, boolean isReadOnly, boolean isHidden,
+                   boolean isSystem, boolean isVolume, boolean isDirectory, boolean isArchive,
+                   Date creationDate, Date lastAccessedDate, Date lastModifiedDate, long size)
+    {
+        this.fileName = fileName;
+        this.fileExtension = fileExtension;
+        this.isReadOnly = isReadOnly;
+        this.isHidden = isHidden;
+        this.isSystem = isSystem;
+        this.isVolume = isVolume;
+        this.isDirectory = isDirectory;
+        this.isArchive = isArchive;
+        this.creationDate = creationDate;
+        this.lastAccessedDate = lastAccessedDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.size = size;
+    }
+
+    public static VFSFile getVFSFile(FileEntry entry)
+    {
+        VFSFile file = new VFSFile();
+        if(!entry.getLongName().equals(""))
+            file.setFileName(entry.getLongName());
+        else
+            file.setFileName(entry.getShortName());
+        file.setFileExtension(entry.getFileExtension());
+        file.setIsReadOnly(entry.isReadOnly());
+        file.setIsHidden(entry.isHidden());
+        file.setIsSystem(entry.isSystem());
+        file.setIsVolume(entry.isVolume());
+        file.setIsDirectory(entry.isDirectory());
+        file.setIsArchive(entry.isArchive());
+        file.setCreationDate(entry.getCreationDate());
+        file.setLastAccessedDate(entry.getLastAccessedDate());
+        file.setLastModifiedDate(entry.getLastModifiedDate());
+        file.setSize(entry.getSize());
+        return file;
+    }
+
+    public String getFileName()
+    {
+        return fileName;
+    }
+
+    public void setFileName(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public String getFileExtension()
+    {
+        return fileExtension;
+    }
+
+    public void setFileExtension(String fileExtension)
+    {
+        this.fileExtension = fileExtension;
+    }
+
+    public boolean isReadOnly()
+    {
+        return isReadOnly;
+    }
+
+    public void setIsReadOnly(boolean isReadOnly)
+    {
+        this.isReadOnly = isReadOnly;
+    }
+
+    public boolean isHidden()
+    {
+        return isHidden;
+    }
+
+    public void setIsHidden(boolean isHidden)
+    {
+        this.isHidden = isHidden;
+    }
+
+    public boolean isSystem()
+    {
+        return isSystem;
+    }
+
+    public void setIsSystem(boolean isSystem)
+    {
+        this.isSystem = isSystem;
+    }
+
+    public boolean isVolume() {
+        return isVolume;
+    }
+
+    public void setIsVolume(boolean isVolume)
+    {
+        this.isVolume = isVolume;
+    }
+
+    public boolean isDirectory()
+    {
+        return isDirectory;
+    }
+
+    public void setIsDirectory(boolean isDirectory)
+    {
+        this.isDirectory = isDirectory;
+    }
+
+    public boolean isArchive() {
+        return isArchive;
+    }
+
+    public void setIsArchive(boolean isArchive)
+    {
+        this.isArchive = isArchive;
+    }
+
+    public Date getCreationDate()
+    {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate)
+    {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastAccessedDate()
+    {
+        return lastAccessedDate;
+    }
+
+    public void setLastAccessedDate(Date lastAccessedDate)
+    {
+        this.lastAccessedDate = lastAccessedDate;
+    }
+
+    public Date getLastModifiedDate()
+    {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate)
+    {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public long getSize()
+    {
+        return size;
+    }
+
+    public void setSize(long size)
+    {
+        this.size = size;
+    }
 }
--- src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VirtualFileSystem.java
+++ src/main/java/com/felhr/usbmassstorageforandroid/filesystems/VirtualFileSystem.java
@@ -1,5 +1,14 @@
 package com.felhr.usbmassstorageforandroid.filesystems;
 
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+
+import com.felhr.usbmassstorageforandroid.filesystems.fat32.FATHandler;
+import com.felhr.usbmassstorageforandroid.filesystems.fat32.FileEntry;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -7,63 +16,94 @@ import java.util.List;
  */
 public class VirtualFileSystem
 {
-    public VirtualFileSystem()
+    private FATHandler fatHandler;
+
+    public VirtualFileSystem(UsbDevice mDevice, UsbDeviceConnection mConnection)
     {
-        //TODO
+        fatHandler = new FATHandler(mDevice, mConnection);
     }
 
     public boolean mount(int index)
     {
-        //TODO
-        return false;
+        return fatHandler.mount(index);
     }
 
     public List<String> list()
     {
-        //TODO
-        return null;
+        List<FileEntry> entries = fatHandler.list();
+        List<String> files = new ArrayList<String>();
+        Iterator<FileEntry> e = entries.iterator();
+        while(e.hasNext())
+        {
+            FileEntry entry = e.next();
+            if(!entry.getLongName().equals(""))
+            {
+                files.add(entry.getLongName());
+            }else
+            {
+                files.add(entry.getShortName());
+            }
+        }
+        return files;
     }
 
     public List<VFSFile> listAll()
     {
-        //TODO
-        return null;
+        List<FileEntry> entries = fatHandler.list();
+        List<VFSFile> files = new ArrayList<VFSFile>();
+        Iterator<FileEntry> e = entries.iterator();
+        while(e.hasNext())
+        {
+            FileEntry entry = e.next();
+            VFSFile file = VFSFile.getVFSFile(entry);
+            files.add(file);
+
+        }
+        return files;
     }
 
     public String getPath()
     {
-        //TODO
-        return null;
+        String path = "/";
+        List<FileEntry> pathDirsEntries = fatHandler.getPath();
+        if(pathDirsEntries.isEmpty())
+            return path;
+        Iterator<FileEntry> e = pathDirsEntries.iterator();
+        while(e.hasNext())
+        {
+            FileEntry entry = e.next();
+            if(!entry.getLongName().equals(""))
+                path += entry.getLongName();
+            else
+                path += entry.getShortName();
+            path += "/";
+        }
+        return path;
     }
 
     public boolean changeDir(String dirName)
     {
-        //TODO
-        return false;
+        return fatHandler.changeDir(dirName);
     }
 
     public boolean changeDirBack()
     {
-        //TODO
-        return false;
+        return fatHandler.changeDirBack();
     }
 
-    public boolean writeFile()
+    public boolean writeFile(File file)
     {
-        //TODO
-        return false;
+        return fatHandler.writeNewFile(file);
     }
 
     public byte[] readFile(String fileName)
     {
-        //TODO
-        return null;
+        return fatHandler.readFile(fileName);
     }
 
     public boolean unMount()
     {
-        //TODO
-        return false;
+        return fatHandler.unMount();
     }
 
 }
